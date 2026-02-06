4f5fac133c142436130773e4660e946434963fe1
nibanovic <nikola.banovic@b-robotized.com>
2026-02-06 10:48:05 +0100

    fix moveit config for nova2 robot

--- nova2_moveit/launch/dobot_moveit.launch.py
+++ nova2_moveit/launch/dobot_moveit.launch.py
@@ -19,7 +19,7 @@ def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("nova2_robot", package_name="nova2_moveit").to_moveit_configs()
 
     ld = LaunchDescription()
-    my_generate_rsp_launch(ld, moveit_config)
+    #my_generate_rsp_launch(ld, moveit_config)
     # 启动move_group
     my_generate_move_group_launch(ld, moveit_config)
     # 启动rviz
